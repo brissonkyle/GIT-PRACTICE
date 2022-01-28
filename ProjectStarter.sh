51c9c8adbadae34a0e8fe603c7d7e01928e28917
Allyson Engwell <kyle.brisson@hotmail.com>
2022-01-27 20:39:38 -0700

    Setting up project

---
+++ ProjectStarter.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+mkdir BasicBash;
+touch index.html;
+echo 'Creating New Project';
+echo 'Hello World' > index.html
+mkdir css
+cd css
+touch style.css;
+cd ..
+mkdir js;
+cd js;
+touch app.js;
+cd ..
+echo 'All Done!';
